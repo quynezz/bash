8b6d97468dba6c5141fa6572a72a3848383e4dc1
quynezz <laungocquyen098@gmail.com>
2026-03-05 10:37:58 +0700

    done

---
+++ basic_globbing.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# '*' match with every keyword
+cp ./glob/*.sh .
+
+# '?' match with every keyword -> could be
+# bar.sh or ber.sh, ...
+mv ./glob/b?r.sh
+
+# more of globbing
+ls -la b?z*.txt
+ls -la ./*.*
+
+# this wil take all of the file start with 'b' and the next char must be 'a', 'r' or 'c'
+ls -al ./b[arc]*
+
+# this will take from range characters
+ls -la b[a-z]*.txt
+
+# move everyfile execept the folder temp itself into temp/
+mv !(temp) temp/
